a753fd8673e562a6cbaaeaabe86429601604cc68
saldanhalopes <rsaldanhalopes@gmail.com>
2023-08-26 12:08:25 -0300

    Start

---
+++ src/main/java/br/com/lablims/spring_lablims/SpringLabLimsApplication.java
@@ -0,0 +1,13 @@
+package br.com.lablims.spring_lablims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLabLimsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLabLimsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/lablims/spring_lablims/SpringLabLimsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lablims.spring_lablims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLabLimsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
